280501f66beba2206fe68be85e26b52037ce310c
tfanto <tomasfanto@gmail.com>
2022-10-03 08:47:37 +0200

    more calcs

--- src/main/java/inkomster/Main.java
+++ src/main/java/inkomster/Main.java
@@ -8,10 +8,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		Calculate calc = new Calculate();
+		Calculate2 calc2 = new Calculate2();
 		try {
-			calc.go("C:/Users/tomas/Documents/transaktions_8.csv",Cst.JUNI);
-			calc.go("C:/Users/tomas/Documents/transaktions_8.csv",Cst.JULI);
-			calc.go("C:/Users/tomas/Documents/transaktions_8.csv",Cst.AUGUSTI);
+			calc.go("C:/Users/tomas/Documents/Lön 2022-09-30 - 2022-01-01.csv",Cst.SEPTEMBER);
+			calc2.go("C:/Users/tomas/Documents/Lön 2022-09-30 - 2022-01-01.csv",Cst.SEPTEMBER);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
